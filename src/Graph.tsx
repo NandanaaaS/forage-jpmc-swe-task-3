0f1c007f3c844645b748e6132c9778442727281f
Nandana S <nandanasreeja26@gmail.com>
2024-06-30 10:35:46 +0530

    done

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
+import { DataManipulator, Row } from './DataManipulator'; // Import Row here
 import './Graph.css';
 
 interface IProps {
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,25 +39,32 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
-  componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+   componentDidUpdate() {
+        if (this.table) {
+      // Ensure that we have at least two data points
+      if (this.props.data.length >= 2) {
+        // Map the data to generate the rows
+        const rows: Row[] = [this.props.data.slice(-2)] // Get the last two ServerRespond objects
+          .map((data: ServerRespond[]) => DataManipulator.generateRow(data)); // Generate the row
+
+        this.table.update(rows as unknown as { [key: string]: any }[]); // Update the table
     }
   }
-}
-
+  }
+ }
 export default Graph;
+
